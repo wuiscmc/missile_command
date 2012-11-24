e63d91cfb62182190429db9c4278782c84ae0f17
Luis Carlos Mateos <lcm@odelay.se>
2012-11-24 15:31:37 +0100

    Basic game logic finished

--- src/org/missile/Canvas.java
+++ src/org/missile/Canvas.java
@@ -2,6 +2,7 @@ package org.missile;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Vector;
@@ -16,7 +17,7 @@ public class Canvas extends JFrame implements Runnable {
 	private boolean shoot;
 	private Vector<Missile> missiles;
 	private Vector<Explosion> explosions;
-
+	private Vector<City> cities;
 	/**
 	 * @param args
 	 */
@@ -26,6 +27,15 @@ public class Canvas extends JFrame implements Runnable {
 		explosions = new Vector<Explosion>();
 		
 		base = new Base(230, 490, 10, 20, 50);
+		
+		cities = new Vector<City>();
+		cities.add(new City(41,450,41,50));
+		int x = 0;
+		for(int i = 0; i< 3; i ++){
+			x = (50 + x) + 50*2;
+			cities.add(new City(x,450,41,50));
+			
+		}
 		
 		setSize(500, 500);
 		setTitle("Missile command");
@@ -63,7 +73,13 @@ public class Canvas extends JFrame implements Runnable {
 			explosions.get(i).draw(g);
 		}
 		
-		
+		for (int i = 0; i < cities.size(); i++) {
+			if(!cities.get(i).getDestroyed())
+				cities.get(i).draw(g);
+			else
+				cities.removeElementAt(i);
+				
+		}
 		
 		for (int i = 0; i < missiles.size(); i++) {
 			missiles.get(i).draw(g);
@@ -89,13 +105,24 @@ public class Canvas extends JFrame implements Runnable {
 			double a = e.getY() - m.getY();
 			double b = e.getX() - m.getX();
 			
-			if ( Math.sqrt((a*a + b*b)) <  e.getR() / 2) {
+			if ( Math.sqrt((a*a + b*b)) <  e.getR() / 3) {
 				m.exploded(true);
 			}
 		}
 	}
 
+	private void reachedCity(Missile m){
+		for(City c: cities){
+			if (c.getRectangle().contains(m.getX(), m.getY())){
+				m.exploded(true);
+				c.setDestroyed(true);
+			}
+				
+		}
+	}
+	
 	private void moveMissiles() {
+		
 		for (int i = 0; i < missiles.size(); i++) {
 			if (missiles.get(i).done()) {
 				Missile m = missiles.get(i);
@@ -103,6 +130,7 @@ public class Canvas extends JFrame implements Runnable {
 				missiles.removeElement(m);
 			} else {
 				missiles.get(i).move();
+				reachedCity(missiles.get(i));
 			}
 		}
 
@@ -114,6 +142,7 @@ public class Canvas extends JFrame implements Runnable {
 				explosions.get(i).move();
 			}
 		}
+		
 	}
 	
 	public void run() {
@@ -128,6 +157,31 @@ public class Canvas extends JFrame implements Runnable {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		}
+	}
+	
+	public class City{
+		private boolean destroyed;
+		private Rectangle r;
+		public City(int x, int y , int w, int h){
+			r = new Rectangle(x,y,w,h);
+			destroyed = false;
+		}
+		
+		public void draw(Graphics g){
+			g.drawRect(r.x, r.y, r.width, r.height);
+		}
+		
+		public Rectangle getRectangle(){
+			return r;
+		}
+		
+		public void setDestroyed(boolean destroyed){
+			this.destroyed = destroyed;
+		}
+		
+		public boolean getDestroyed(){
+			return destroyed;
 		}
 	}
 }
